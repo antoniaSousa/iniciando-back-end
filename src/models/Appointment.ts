9344f4569bbabf9c111104b2dd24cb11c2d02566
antonia <antonia@unifesspa.edu.br>
2020-09-01 20:28:19 -0300

    Aula modulo 2 Trabalhando com dados

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,16 +1,21 @@
 import { uuid } from "uuidv4";
 
-class Appointments{
+interface AppointmentConstructor {
+    provider: string;
+    date: Date;
+}
+
+class Appointment{
     id: String;
 
     provider: String;
 
     date: Date;
 
-    constructor (provider: String, date: Date){
+    constructor ({provider, date}:Omit<Appointment, 'id'>){
         this.id = uuid();
         this.provider =provider;
         this.date = date;
     }
 }
-export default Appointments;
+export default Appointment;
--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,7 +1,13 @@
 
 import Appointment from '../models/Appointment';
 import { isEqual } from 'date-fns';
+
+interface CreateAppointmentDTO{
+    provider: string;
+    date:Date;
+}
 class
+
 AppointmentsRepository{
     private appointments: Appointment[];
 
@@ -19,8 +25,8 @@ AppointmentsRepository{
          return findApponitment || null;
      }
 
-    public create (provider: string, date: Date): Appointment{
-        const appointment = new Appointment(provider, date);
+    public create ({provider, date}: CreateAppointmentDTO):Appointment{
+        const appointment = new Appointment({provider, date});
 
         this.appointments.push(appointment);
 
