96402a199d38808f861c755e2065dc672c9adc0c
antonia <antonia@unifesspa.edu.br>
2020-09-01 21:42:26 -0300

    Aula modulo 2 Services & SOLID

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
-import {Router, request, response} from 'express';
-import {startOfHour, parseISO} from 'date-fns';
+import {Router} from 'express';
+import { parseISO} from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,22 +14,20 @@ appointmentsRouter.get('/', (request, response)=>{
 });
 
 appointmentsRouter.post('/', (request, response) =>{
-
+try{
     const {provider,date} =  request.body;
+    const parseDate = parseISO(date);
 
-    const parseDate = startOfHour(parseISO(date));
+    const createAppointment = new CreateAppointmentService(
+        appointmentsRepository,
+    );
+    const appointment = createAppointment.execute({date: parseDate, provider});
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-       parseDate
-        );
-
-        if (findAppointmentInSameDate){
-            return response
-            .status(400)
-            .json({message: 'This appointment is already booked'});
-        }
-    const appointment = appointmentsRepository.create(provider, parseDate);
     return response.json({appointment});
+} catch (err){
+    return response.status(400).json({error: err.message})
+}
+
 });
 
 export default appointmentsRouter;
