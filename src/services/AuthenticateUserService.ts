a8b855d6c566606dcbfc08b6c7265e7e884b2451
antonia <antonia@unifesspa.edu.br>
2020-09-07 09:12:56 -0300

    Finalizando a autenticação do usuário

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from "typeorm";
+import User from "../models/User";
+import { compare } from "bcryptjs";
+import { sign } from 'jsonwebtoken';
+
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response{
+    user: User;
+    token: string;
+}
+class AuthenticateUserServive {
+public async execute({ email, password }: Request): Promise<Response>{
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: {email}});
+
+    if (!user){
+        throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare( password, user.password);
+
+    if(!passwordMatched){
+        throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({},'95a6e64a687f846024a5e63f4b5b4b5d', {
+        subject: user.id,
+        expiresIn: '1d',
+    });
+
+     return {
+         user,
+         token,
+     };
+
+}
+}
+export default AuthenticateUserServive;
